8cbb2fb80ae21b24fe10e66525a0cb5cc299fe63
Mohammad Khodadad <khodadadmohammad1997@gmail.com>
2024-10-12 19:36:05 -0400

    updated the data loader

---
+++ BreakHis/load_data.py
@@ -0,0 +1,14 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+import os
+import glob
+
+def load_breakhis_data(data_address):
+    addresses=glob.glob(os.path.join(data_address,'*/*/*/*/*/*/*/40X/*.png'))
+    data=[Image.open(address).convert('RGB') for address in addresses[:10]]
+    labels=[address.replace('\\','/').split('/')[4] for address in addresses[:10]]
+    return data,labels
+
+if __name__ == "__main__":
+    load_breakhis_data('data')
