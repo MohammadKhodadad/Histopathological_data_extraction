49273216d5256f866f1bfc65c21bb558d0c9e16c
Mohammad Khodadad <khodadadmohammad1997@gmail.com>
2024-10-14 11:46:16 -0400

    updated the code for model

--- BACH/load_data.py
+++ BACH/load_data.py
@@ -9,6 +9,8 @@ def load_bach_data(data_address):
     images=data['train']['image']+data['test']['image']
     labels=data['train']['label']+data['test']['label']
     split=['train' for _ in range(len(data['train']['label']))] + ['test' for _ in range(len(data['test']['label']))]
+    print(f"number of images: {len(labels)}")
+    print(images[0].size)
     return images,labels,split
     
 if __name__ == "__main__":
